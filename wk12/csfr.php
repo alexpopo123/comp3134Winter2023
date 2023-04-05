a3ad7fb34733f13fed2cf028b48caf1e47c8b47f
alex tsang <alextsangjoker888@gmail.com>
2023-04-04 23:29:53 -0400

    wk7andwk12

---
+++ wk12/csfr.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>CSFR</title>
+    <meta name="description" content="description"/>
+    <meta name="author" content="author" />
+    <meta name="keywords" content="keywords" />
+    <style type="text/css">.body { width: auto; }</style>
+  </head>
+  <body>
+
+	<form method='post'>
+
+
+	<label>Username</label>
+<br/>
+<input type='text' name='username' />
+<br/>
+<label>Password</label>
+<br/>
+<input type='password' name='password'/>
+<br/>
+<input type='submit' name='submit' value='Submit form'/>
+	</form>
+
+  </body>
+</html>
---
+++ wk12/csfr_action.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>CSFR Action</title>
+    <meta name="description" content="description"/>
+    <meta name="author" content="author" />
+    <meta name="keywords" content="keywords" />
+    <style type="text/css">.body { width: auto; }</style>
+</head>
+<body>
+    <h1>CSFR Action</h1>
+    <p>CSFR form submission successful!</p>
+</body>
+</html>
